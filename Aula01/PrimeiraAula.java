b4fb582f11e9c154b322ec41d0a168f6586df25c
vinicius-campelo <vilog_6@msn.com>
2021-03-28 22:49:41 -0400

    Add arquivos

---
+++ Aula01/PrimeiraAula.java
@@ -0,0 +1,25 @@
+
+/**
+ * Escreva a descrição da classe PrimeiraAula aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class PrimeiraAula
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    public String x;
+
+    public PrimeiraAula()
+    {
+        x = "ola meu nome e vinicius";
+        System.out.println(x);
+    }
+    
+    
+    public static void main(String args[]){
+        
+        PrimeiraAula aula = new PrimeiraAula();
+        
+    }
+}
